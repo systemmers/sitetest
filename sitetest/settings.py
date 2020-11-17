dc31cd0e0dcf621384d2a19bb343062cf2a820d7
systemmers <74598147+systemmers@users.noreply.github.com>
2020-11-18 01:33:33 +0900

    Update settings.py

--- sitetest/settings.py
+++ sitetest/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'sitetest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR.joinpath('db.sqlite3')),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
